91b6ec2a9323fda506127b77fd8ea8d2d2c50166
Bryce Gattis <bag16b@acu.edu>
2018-02-08 00:15:13 -0600

    Working uploading 2 specific images from Virtual Device. Only works on these 2 images right now, they are hardcoded.

--- app/src/main/java/com/cs316/meme/AddToDatabase.java
+++ app/src/main/java/com/cs316/meme/AddToDatabase.java
@@ -1,15 +1,28 @@
 package com.cs316.meme;
 
+import android.*;
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,7 +30,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.util.ArrayList;
 
 
 /**
@@ -28,21 +49,41 @@ public class AddToDatabase extends AppCompatActivity{
 
     private static final String TAG = "AddToDatabase";
 
-    private Button mAddtoDB;
+    private ImageView image;
+    private EditText imageName;
+    private Button btnUpload, btnNext, btnBack;
+    private ProgressDialog mProgressDialog;
 
-    private EditText mNewFood;
+    private final static int mWidth = 512;
+    private final static int mLength = 512;
+
+    private ArrayList<String> pathArray;
+    private int array_position;
+
+    private StorageReference mStorageRef;
 
     private FirebaseDatabase mFirebaseDatabase;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference myRef;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_to_database_layout);
 
-        mAddtoDB = (Button) findViewById(R.id.btnAddNewFood);
-        mNewFood = (EditText) findViewById(R.id.add_new_food);
+        image = (ImageView) findViewById(R.id.uploadImage);
+        btnBack = (Button) findViewById(R.id.btnBackImage);
+        btnNext = (Button) findViewById(R.id.btnNextImage);
+        btnUpload = (Button) findViewById(R.id.btnUploadImage);
+        imageName = (EditText) findViewById(R.id.imageName);
+        pathArray = new ArrayList<>();
+        mProgressDialog = new ProgressDialog(AddToDatabase.this);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        checkFilePermissions();
+        addFilePaths();
 
         mAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
@@ -81,23 +122,96 @@ public class AddToDatabase extends AppCompatActivity{
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-        mAddtoDB.setOnClickListener(new View.OnClickListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: Attempting to add object to database.");
-                String newFood = mNewFood.getText().toString();
-                if(!newFood.equals("")){
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    String userID = user.getUid();
-                    myRef.child(userID).child("Food").child("Favorite Foods").child(newFood).setValue("true");
-                    toastMessage("Adding " + newFood + " to database...");
-                    //reset the text
-                    mNewFood.setText("");
+                if(array_position > 0){
+                    Log.d(TAG, "onClick: Back an Image.");
+                    array_position = array_position - 1;
+                    loadImageFromStorage();
+                }
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(array_position < pathArray.size() - 1){
+                    Log.d(TAG, "onClick: Next Image.");
+                    array_position = array_position + 1;
+                    loadImageFromStorage();
+                }
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Uploading Image.");
+                mProgressDialog.setMessage("Uploading Image...");
+                mProgressDialog.show();
+
+                FirebaseUser user = mAuth.getCurrentUser();
+                String userID = user.getUid();
+
+                String name = imageName.getText().toString();
+                if(!name.equals("")){
+                    Uri uri = Uri.fromFile(new File(pathArray.get(array_position)));
+                    StorageReference storageReference = mStorageRef.child("images/users/" + userID + "/" + name + ".jpg");
+                    storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            toastMessage("Upload Success");
+                            mProgressDialog.dismiss();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            toastMessage("Upload Failed");
+                            mProgressDialog.dismiss();
+                        }
+                    });
                 }
 
             }
         });
     }
+
+    private void addFilePaths(){
+        Log.d(TAG, "addFilePaths: Adding file paths.");
+        //String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
+        String path = "/storage/emulated/0/DCIM/Camera/IMG_20180208_054917.jpg";
+        pathArray.add(path+"");
+        pathArray.add("/storage/emulated/0/DCIM/Restored/KH.jpg");
+        loadImageFromStorage();
+    }
+
+    private void loadImageFromStorage() {
+
+        try{
+            String path = pathArray.get(array_position);
+            File f = new File(path,"");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            image.setImageBitmap(b);
+        }
+        catch(FileNotFoundException e){
+            Log.e(TAG, "loadImageFromStorage: FileNotFoundException: " + e.getMessage() );
+        }
+
+    }
+
+    private void checkFilePermissions(){
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = AddToDatabase.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
+            permissionCheck += AddToDatabase.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+            if(permissionCheck != 0) {
+                this.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+
+        }
+        else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
+
 
     @Override
     public void onStart() {
