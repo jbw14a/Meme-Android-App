3808b958478e5f5ba4b3ac794a71a8918fc8ffc7
Bryce Gattis <bag16b@acu.edu>
2018-02-14 16:19:02 -0600

    Template for TakePhoto.java, working downloading from firebase in ViewMemes

--- app/src/main/java/com/cs316/meme/AddToDatabase.java
+++ app/src/main/java/com/cs316/meme/AddToDatabase.java
@@ -177,6 +177,7 @@ public class AddToDatabase extends AppCompatActivity{
 
                 }
 
+
             }
         });
     }
--- app/src/main/java/com/cs316/meme/TakePhoto.java
+++ app/src/main/java/com/cs316/meme/TakePhoto.java
@@ -1,29 +1,106 @@
 package com.cs316.meme;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
 
 public class TakePhoto extends AppCompatActivity {
+
+    private static final String TAG = "TakePhoto";
+
 
     private ImageView image;
     private static final int CAMERA_REQUEST = 123;
     private Bitmap photo;
+    private Button btnUpload;
+    private EditText imageName;
+
+    private ArrayList<String> pathArray;
+
+    private StorageReference mStorageRef;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private ProgressDialog mProgressDialog;
+
+    private int array_position = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
         image = (ImageView) findViewById(R.id.uploadImage);
+        imageName = (EditText) findViewById(R.id.imageName);
+        btnUpload = (Button) findViewById(R.id.btnUploadImage);
+        mAuth = FirebaseAuth.getInstance();
+        mProgressDialog = new ProgressDialog(TakePhoto.this);
+        pathArray = new ArrayList<>();
 
+
+
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Uploading Image.");
+//                mProgressDialog.setMessage("Uploading Image...");
+//                mProgressDialog.show();
+
+                String name = imageName.getText().toString();
+
+                FirebaseUser user = mAuth.getCurrentUser();
+                String userID = user.getUid();
+
+                if (!name.equals("")) {
+                    Log.d(TAG, "Proceeding...");
+                    Uri uri = Uri.fromFile(new File(pathArray.get(array_position)));
+                    StorageReference storageReference = mStorageRef.child("images/users/" + userID + "/" + name + ".jpg");
+                    storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            toastMessage("Upload Success");
+                            mProgressDialog.dismiss();
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            toastMessage("Upload Failed");
+                            mProgressDialog.dismiss();
+                        }
+                    });
+
+
+                }
+
+            }
+        });
     }
-
     public void takePhoto(View v){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, CAMERA_REQUEST);
@@ -47,8 +124,10 @@ public class TakePhoto extends AppCompatActivity {
 
             Uri savedImageURI = Uri.parse(saveImageURL);
             image.setImageURI(savedImageURI);
-
         }
 
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
